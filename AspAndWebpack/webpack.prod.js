013e39076cc42d6ce5f92506fb670b7963e82ee5
Jonathan Harrison <j.harrison@altitudeangel.com>
2017-09-18 08:26:09 +0100

    copied everything over

---
+++ AspAndWebpack/webpack.prod.js
@@ -0,0 +1,24 @@
+﻿const webpack = require("webpack");
+const Merge = require("webpack-merge");
+const CommonConfig = require("./webpack.common.js");
+
+module.exports = Merge(CommonConfig, {
+    devtool: "hidden-source-map",
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: false,
+            mangle: {
+                screw_ie8: true,
+                keep_fnames: true
+            },
+            compress: {
+                screw_ie8: true
+            },
+            comments: false
+        })
+    ]
+})
